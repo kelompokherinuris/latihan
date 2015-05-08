8af15d8279b813a904d37bbe673366b5cabf3023
SBC4 <SBC4@SBC10>
2015-05-08 17:54:56 +0700

    penjumlahan

---
+++ src/penjumlahan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SBC4
+ */
+
+ public class penjumlahan {  
+         
+        public static void main(String[] args) {  
+        int bil1, bil2, hasil;  
+        bil1 = 10;  
+        bil2 = 500;  
+        hasil = bil1 + bil2;  
+        System.out.println("Hasil Penjumlahan Bilangan = ");  
+        System.out.println(hasil);  
+        }  
+    }  
